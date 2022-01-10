13a6e6fcb936aeef04ad62b3f630d66b2990adb3
Arthur.R.A <82613934+Arthur-All@users.noreply.github.com>
2022-01-10 15:57:26 -0800

    Sucessor-par

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class DIO
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y;
+        if (x % 2 == 0)
+        {
+            y = x + 2;
+        }
+        else
+        {
+            y = x + 1;
+        }
+        Console.WriteLine(y);
+    }
+}
